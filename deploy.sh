1f1aaa64016fb8915802383db2d3c91b666c12d2
llYOrchll <left4deadjgo@hotmail.com>
2024-02-28 10:42:03 -0600

    Agregado deploy
    Agregado deploy con ngrok

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+echo "Iniciando deploy"
+sudo systemctl stop nginx
+
+sudo git pull origin main
+sudo systemct start nginx
+ngrok http 80 > /dev/null &
+sleep 10
+ngrok_url=$(curl -s http:localhost:4040/api/tunnels | -r '.tunnels[0].public.url')
+echo "La pagina esta lista en la URL: $ngrok_url"
