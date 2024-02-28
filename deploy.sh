fddd22caad779049ad504a3fe9167900cd55f34e
llYOrchll <left4deadjgo@hotmail.com>
2024-02-28 10:50:30 -0600

    Update deploy.sh
    actualizado

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ sudo git pull origin main
 sudo systemctl start nginx
 ngrok http 80 > /dev/null &
 sleep 10
-ngrok_url=$(curl -s http:localhost:4040/api/tunnels | -r '.tunnels[0].public.url')
+ngrok_url=$(curl -s http:localhost:4040/api/tunnels | -r '.tunnels[0].public_url')
 echo "La pagina esta lista en la URL: $ngrok_url"
